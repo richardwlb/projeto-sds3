286caaaa3d599fbeb1223ca2633fa712c20dfdd1
Richard W. L. Brehmer <richard@bnu.com.br>
2021-05-19 19:53:42 -0300

    feat: Built entities database on backend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,37 @@
-import React from 'react';
+import BarChart from 'components/BarChart';
+import DataTable from 'components/DataTable';
+import DonutChart from 'components/DonutChart';
+import Footer from 'components/Footer';
+import NavbBar from 'components/NavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <h1>Dev Superior</h1>
-      </header>
-    </div>
+    <>
+      <NavbBar />
+      <div className="container">
+        <h1 className="text-primary py-3">Dascboard de vendas</h1>
+        <div className="row px-3">
+
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary" >Taxa de sucesso (%)</h5>
+            <BarChart />
+          </div>
+
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary" >Todas as vendas</h5>
+            <DonutChart />
+          </div>
+          
+        </div>
+
+        <div className="py-3">
+          <h2 className="text-primary">Todas vendas</h2>
+        </div>
+
+        <DataTable />
+      </div>
+      <Footer />
+    </>
   );
 }
 
